f2365a581520ad0d7529ff15908bd1cf53cc5133
Sebastián Iñones <sebainones@gmail.com>
2020-06-04 11:27:28 +0200

    A lot of refactoring and also setting and reading metadata

--- NoSqlApp/Program.cs
+++ NoSqlApp/Program.cs
@@ -5,44 +5,92 @@ using System;
 using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace NoSqlApp
 {
     class Program
     {
-
+        private const string MetadataVehcileType = "vehicleType";
         private static string BlockBlobName;
         private static string ContainerName;
 
+        // Frist get container Reference
+        static CloudBlobContainer ContainerReference => Shared.CloudBlobClient.GetContainerReference(ContainerName);
+        static CloudBlockBlob CurrentCloudBlockBlob;
+
+
         public static void Main(string[] args)
         {
+            InitializeConfiguration();
+
             MainAsync().Wait();
         }
 
-        static async Task MainAsync()
+        private static void InitializeConfiguration()
         {
-
             IConfigurationBuilder configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
             IConfigurationRoot config = configBuilder.Build();
 
             ContainerName = config["BlobContainerName"];
             BlockBlobName = config["BlockBlobName"];
-
-            byte[] imageByteArray = GetImageByteArray();
-
-            await Program.UploadImageAsync(imageByteArray, BlockBlobName);
-
-            await Program.DeleteImageAsync(BlockBlobName);
         }
 
-        private static async Task DeleteImageAsync(string blockBlobName)
+        static async Task MainAsync()
+        {
+            byte[] imageByteArray = GetImageByteArray();
+
+            CurrentCloudBlockBlob = await GetBlockBlobAsync(BlockBlobName);
+
+            if (CurrentCloudBlockBlob != null)
+            {
+                await UploadImageAsync(imageByteArray);
+
+                await SetMetadataAsync("sedan");
+
+                await FetchBlobInformation();
+
+                await DeleteImageAsync();
+            }
+            else
+            {
+                WriteLine("There is no Blob");
+            }
+        }
+
+        private static async Task<CloudBlockBlob> GetBlockBlobAsync(string blockBlobName)
+        {
+            //DatabaseResponse databaseResponse = await Shared.Client.CreateDatabaseIfNotExistsAsync(DatabaseId);
+
+            //Till this point NO server side request have been made!!!
+            await ContainerReference.CreateIfNotExistsAsync(BlobContainerPublicAccessType.Blob, null, null);
+
+            //Now that is has been created
+            return ContainerReference.GetBlockBlobReference(blockBlobName);
+
+        }
+
+        private static async Task FetchBlobInformation()
+        {
+            await CurrentCloudBlockBlob.FetchAttributesAsync();
+            WriteLine(CurrentCloudBlockBlob.Metadata[MetadataVehcileType]);
+
+        }
+
+        private async static Task SetMetadataAsync(string vehicleType)
+        {
+            CurrentCloudBlockBlob.Metadata[MetadataVehcileType] = vehicleType;
+
+            await CurrentCloudBlockBlob.SetMetadataAsync();
+
+        }
+
+        private static async Task DeleteImageAsync()
         {
             try
             {
-                CloudBlobContainer containerReference = Shared.CloudBlobClient.GetContainerReference(ContainerName);
-                
-                CloudBlockBlob cloudBlockBlob = containerReference.GetBlockBlobReference(blockBlobName);
-                await cloudBlockBlob.DeleteIfExistsAsync();
+                await CurrentCloudBlockBlob.DeleteIfExistsAsync(); ;
+
             }
             catch (Exception e)
             {
@@ -50,6 +98,7 @@ namespace NoSqlApp
                 throw;
             }
         }
+
         private static byte[] GetImageByteArray()
         {
             byte[] imageByteArray;
@@ -67,33 +116,20 @@ namespace NoSqlApp
         /// <summary>
         /// Create the Containter if it does not exist and then upload an image as a blob
         /// </summary>
-        private static async Task<Uri> UploadImageAsync(byte[] imageByteArray, string blobName)
+        private static async Task UploadImageAsync(byte[] imageByteArray)
         {
             try
             {
-                // Frist get container Reference
-                CloudBlobContainer containerReference = Shared.CloudBlobClient.GetContainerReference(ContainerName);
-
-                //DatabaseResponse databaseResponse = await Shared.Client.CreateDatabaseIfNotExistsAsync(DatabaseId);
-
-                //Till this point NO server side request have been made!!!
-                await containerReference.CreateIfNotExistsAsync(BlobContainerPublicAccessType.Blob, null, null);
-
-                //Now that is has been created
-                CloudBlockBlob cloudBlockBlob = containerReference.GetBlockBlobReference(blobName);
                 //If we know it is an image
                 //https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
-                cloudBlockBlob.Properties.ContentType = "image/png";
+                CurrentCloudBlockBlob.Properties.ContentType = "image/png";
 
-
-                var isExistentBlob = await cloudBlockBlob.ExistsAsync();
+                var isExistentBlob = await CurrentCloudBlockBlob.ExistsAsync();
 
                 if (!isExistentBlob)
-                    await cloudBlockBlob.UploadFromByteArrayAsync(imageByteArray, 0, imageByteArray.Length);
+                    await CurrentCloudBlockBlob.UploadFromByteArrayAsync(imageByteArray, 0, imageByteArray.Length);
                 else
-                    Console.WriteLine($"Blob {cloudBlockBlob.Name} already exists in this URI: {cloudBlockBlob.Uri}");
-
-                return cloudBlockBlob.Uri;
+                    WriteLine($"Blob {CurrentCloudBlockBlob.Name} already exists in this URI: {CurrentCloudBlockBlob.Uri}");
 
             }
             catch (Exception e)
